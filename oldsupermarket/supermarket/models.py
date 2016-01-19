80f82bc60beb135104bfa1917ee44e8fc16c3a59
Mutungi <ianmutungi00@gmail.com>
2016-01-19 01:56:02 -0600

    changes in models.py spelling check of IntegerField

--- oldsupermarket/supermarket/models.py
+++ oldsupermarket/supermarket/models.py
@@ -7,16 +7,16 @@ class Products(models.model):
 	Product_name = models.CharField(max_length=30)
 	Short_Description = models.TextField(max_length=50)
 	Long_Description = models.TextField(max_length=500)
-	Min_amount_product = models.IntergerField(default=0)
-	Cost_Price = models.IntergerField(default=0)
-	Sale_price = models.IntergerField(default=0)
+	Min_amount_product = models.IntegerField(default=0)
+	Cost_Price = models.IntegerField(default=0)
+	Sale_price = models.IntegerField(default=0)
 
 class Sales(models.model):
 	Product_name = models.ForeignKey(Products)
 	Short_Description =models.ForeignKey(Products)
 	Sale_price = models.ForeignKey(Products)
-	Date_of_sale = models.DatetimeField()
-	Number_of_Documents = models.IntergerField(default=0)
+	Date_of_sale = models.DateTimeField()
+	Number_of_Documents = models.IntegerField(default=0)
 
 	
 
