8d59d7683cb9c55594b8e44b26e91aa46036885a
Mutungi <ianmutungi00@gmail.com>
2016-01-19 02:41:29 -0600

    Updates

--- oldsupermarket/supermarket/models.py
+++ oldsupermarket/supermarket/models.py
@@ -1,5 +1,7 @@
+import datetime
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 
 
@@ -10,12 +12,20 @@ class Products(models.model):
 	Min_amount_product = models.IntegerField(default=0)
 	Cost_Price = models.IntegerField(default=0)
 	Sale_price = models.IntegerField(default=0)
+        def __str__(self):
+        	return self.Product_name
+        def __str__(self):
+        	return self.Cost_Price
 
 class Sales(models.model):
 	Product_name = models.ForeignKey(Products)
 	Date_of_sale = models.DateTimeField()
 	Number_of_Documents = models.IntegerField(default=0)
-
+        def __str__(self):
+        	return self.Date_of_sale >= timezone.now() - datetime.timedelta(days=1)
+        def __str__(self):
+        	return self.Number_of_Documents
+        	
 	
 
 
