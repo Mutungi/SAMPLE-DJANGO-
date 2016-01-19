e9c230d755bfba9ef8929e833eda4d36df945795
Mutungi <ianmutungi00@gmail.com>
2016-01-19 01:10:08 -0600

    Class Sales Attributes Update
    Note that three fields are foreign keys in Class products

--- oldsupermarket/supermarket/models.py
+++ oldsupermarket/supermarket/models.py
@@ -12,6 +12,11 @@ class Products(models.model):
 	Sale_price = models.IntergerField(default=0)
 
 class Sales(models.model):
+	Product_name = models.ForeignKey(Products)
+	Short_Description =models.ForeignKey(Products)
+	Sale_price = models.ForeignKey(Products)
+	Date_of_sale = models.DatetimeField()
+	Number_of_Documents = models.IntergerField(default=0)
 
 	
 
