9a831f3049d545822068bc1598bc561391f7af2c
Mutungi Ian <ianmutungi00@gmail.com>
2016-01-19 09:46:56 -0600

    The Whole project to work on so check the model.py file for the start

---
+++ oldsupermarket/supermarket/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+
+class Products(models.model):
+	Product_name = models.CharField(max_length=30)
+	Short_Description = models.TextField(max_length=50)
+	Long_Description = models.TextField(max_length=500)
+	Min_amount_product = models.IntergerField(default=0)
+	Cost_Price = models.IntergerField(default=0)
+	Sale_price = models.IntergerField(default=0)
+
+class Sales(models.model):
+	
+
+
+
